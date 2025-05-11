618391d5694179238acc69414d70e9ea70b4fb9c
Karen Zhao <146024684+karenz710@users.noreply.github.com>
2025-05-11 14:12:03 +0000

    case2

--- src/server/game_logic.c
+++ src/server/game_logic.c
@@ -76,9 +76,6 @@ void reset_game_state(game_state_t *game) {
     }
     game->next_card = 0;
     for (int i = 0; i < 5; i++) {
-        game->player_stacks[i] = 0;
-    }
-    for (int i = 0; i < 5; i++) {
         game->current_bets[i] = 0;
     }
     game->highest_bet = 0;
